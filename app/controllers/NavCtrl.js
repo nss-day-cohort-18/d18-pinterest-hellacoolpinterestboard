437d63fcb2b133e6b2947bfad2e634a8cc7f5051
Helana <helanan@gmail.com>
2017-03-01 14:45:21 -0600

    added img and nav ctrl

---
+++ app/controllers/NavCtrl.js
@@ -0,0 +1,21 @@
+"use strict";
+
+"use strict";
+
+app.controller("NavCtrl", function($scope, $window, AuthFactory){
+  // $scope.searchText = SearchTermData; inject SearchTermData once Auth is hooked up
+  $scope.isLoggedIn = false;
+
+  firebase.auth().onAuthStateChanged( function(user){
+		if (user) {
+			$scope.isLoggedIn = true;
+			console.log("currentUser logged in", user, $scope.isLoggedIn);
+			$scope.$apply();
+		}else{
+			$scope.isLoggedIn = false;
+			console.log("currentUser logged in", $scope.isLoggedIn);
+			$window.location.href = "#!/login"; //TO DO: need to decide where this $location directs the user after they login
+		}
+	});
+
+});
