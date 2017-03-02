1a98faee54df9245947a60debf7c38ba2fd7c2ea
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-03-02 10:57:29 -0600

    Finished registration ctrl. YOU CAN NOW SIGN UP WITH FIREBASE

--- app/app.js
+++ app/app.js
@@ -2,3 +2,51 @@
 
 var app = angular.module("HellaCoolPinterestProject", ["ngRoute"]);
 
+//resolve for the app to check whether or not a user is logged in
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+	console.log("I am here");
+	AuthFactory.isAuthenticated()
+		.then((userExists) => {
+			console.log("userExists", userExists);
+			if (userExists) {
+				AuthFactory.changeLogin(true);
+				console.log("Authenticated, go ahead");
+				resolve();
+			} else {
+				console.log("Authentication rejected, go away.");
+				reject();
+			}
+		});
+});
+
+
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/register', {
+			templateUrl: '/partials/Register.html',
+			controller: 'RegisterCtrl',
+		})
+		.when('/explore', {
+			templateUrl: '/partials/Explore.html',
+			controller: 'ExploreCtrl',
+		})
+		.otherwise('/register');
+});
+
+app.run(($location, FBCreds) => {
+	console.log("Here is your authConfig within app.js: ", FBCreds);
+	firebase.initializeApp(FBCreds);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
